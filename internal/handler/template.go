f4355e6e4be267d03b1c8f71c10520e6bd8824f4
evecus <jamie55@163.com>
2026-02-22 18:15:38 +0800

    Update template.go

--- internal/handler/template.go
+++ internal/handler/template.go
@@ -233,7 +233,7 @@ const indexHTML = `<!DOCTYPE html>
         <label data-i18n="label_username">用户名</label>
         <div class="input-wrap">
           <svg class="input-icon" width="15" height="15" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"><path d="M20 21v-2a4 4 0 0 0-4-4H8a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></svg>
-          <input type="text" id="username" data-i18n-ph="ph_username" placeholder="root（默认）"/>
+          <input type="text" id="username" data-i18n-ph="ph_username" placeholder="root"/>
         </div>
       </div>
       <div class="field full">
